52044109cc2649ae379b0f38202419c9c7a0d1d6
Greg Ball <gregball@Gregs-MBP.attlocal.net>
2021-12-09 18:08:43 -0500

    mostly styled my page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledFriend = styled.div
+`
+.title {
+    color: #B2B1B9;
+    background-color: #2C2E43;
+    padding: 5%;
+    display: flex;
+    text-align: center;
+    justify-content: center;
+}
+
+.date {
+    color: #B2B1B9;
+    background-color: #2C2E43;
+    padding: 5%;
+    display: flex;
+    text-align: center;
+    justify-content: center;
+}
+    
+}
+img {
+    max-width:80%;
+    height:20%;
+
+   
+}
+
+.explanation {
+    color: #B2B1B9;
+    background-color: #2C2E43;
+    padding: 5%;
+    display: flex;
+    text-align: center;
+}
+`
 
 const Card = (props) => {
 
     return (
-        <div className='card-container'>
+        <StyledFriend>
             <h1 className='title'>{props.data.title}</h1>
             <h2 className='date'>{props.data.date}</h2>
             <img src = {props.data.hdurl} alt='Space'/>
             <p className='explanation'>{props.data.explanation}</p>
-        </div>
+        </StyledFriend>
     )
 }
 
