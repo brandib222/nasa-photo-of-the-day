3b3c37883bc6cf6697f84500c952ed1e788efbfb
Greg Ball <gregball@Gregs-MBP.attlocal.net>
2021-12-09 18:47:50 -0500

    finished and its beautiful

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const StyledFriend = styled.div
 .title {
     color: #B2B1B9;
     background-color: #595260;
-    background: url('https://images.unsplash.com/photo-1528722828814-77b9b83aafb2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3BhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60') no-repeat center center;
+    background: url('https://images.unsplash.com/photo-1505322022379-7c3353ee6291?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fHNwYWNlJTIwaGR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60') no-repeat center center;
 	background-size: cover;
 	color: #fff;
 	-webkit-text-fill-color: transparent;
@@ -18,7 +18,7 @@ const StyledFriend = styled.div
     justify-content: center;
     box-shadow: 5px 10px #FFD523;
     font-size: 90px;
-    
+    font-weight: 900;
 }
 
 .date {
@@ -29,10 +29,10 @@ const StyledFriend = styled.div
     text-align: center;
     justify-content: center;
     background-color: #e5e5f7;
-    opacity: 0.8;
-    background: url('https://images.unsplash.com/photo-1528722828814-77b9b83aafb2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3BhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60') no-repeat center center;
+    opacity: 95%;
+    background: url('https://images.unsplash.com/photo-1532798369041-b33eb576ef16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzd8fHNwYWNlJTIwaGR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60') no-repeat center center;
     background-size: cover;
-    box-shadow: 0 0 15px;
+    box-shadow: 0 0 20px;
 
 }
     
@@ -51,6 +51,7 @@ img {
     padding: 5%;
     display: flex;
     text-align: center;
+    box-shadow: 0 0 10px;
 }
 `
 
